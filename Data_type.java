1997a50d7f10603bbd2ca4f2b1be8147254832b3
Yattan-deep <ysing@armada.ai>
2024-07-12 12:35:56 +0530

    Data_type defaults value

---
+++ Data_type.java
@@ -0,0 +1,17 @@
+public class Data_type 
+{
+    public static void main(String[] agr )
+    {
+        byte num1 = 10;  // 8 bit
+        short num2 = 20; // 16 bit
+        int num3 = 10; // 32 bit
+        long num4 = 10; // 64 bit 
+
+        float fnum = 10.0f; // 32 bit  ------ NOT Default 
+        double dnum = 10.0; // 64 bit -------Default 
+
+        char c = 'J'; // 16 bits ----- UNICODE (2 BYTES)
+
+        boolean b = true; // not specified defends upon JVM
+    }
+}
